fe0315ec8d1f63ca68a2951d1d796137f62646d0
benjamin.laplane <--show-origin>
2019-01-01 17:14:34 +0100

    add tests

---
+++ step1/conftest.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# __author__ = 'Benjamin'
+
+# file needed for pytest to run
+# https://stackoverflow.com/questions/10253826/path-issue-with-pytest-importerror-no-module-named-yadayadayada
