335cd91c5ceaf1c94e85690851af49431419e408
benjamin.laplane <--show-origin>
2019-02-12 13:41:55 +0100

    update for build files

--- clean.sh
+++ clean.sh
@@ -6,3 +6,9 @@ rmdir step*/__pycache__
 rm step*/tests/__pycache__/*
 rmdir step*/tests/__pycache__
 rm step*/*.retry
+rm -r step*/roles/redfish/files/build/*
+rmdir step*/roles/redfish/files/build
+rm -r step*/roles/redfish/files/dist/*
+rmdir step*/roles/redfish/files/dist
+rm -r step*/roles/redfish/files/*egg*/*
+rmdir step*/roles/redfish/files/*egg*
