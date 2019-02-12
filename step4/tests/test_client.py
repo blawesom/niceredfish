552ce4aa27c457836f88b247fcb6359931230b7c
benjamin.laplane <--show-origin>
2019-02-12 13:35:45 +0100

    SSL default configuration as production

--- step4/tests/test_client.py
+++ step4/tests/test_client.py
@@ -8,31 +8,28 @@ import requests
 import random
 import time
 
-with open('inventory', 'r') as inv:
-    ip = inv.readlines()[1]
+domain = 'redfish.blawesom.com'
 
 lines = open('/usr/share/dict/words').read().splitlines()
 new_name =random.choice(lines)
 
-# s = requests.Session()
-# s.verify = '/tmp/cert.pem'
 
 def test_root():        
-    response = requests.get('https://{}/'.format(ip), verify=False)
+    response = requests.get('https://{}/'.format(domain))
     data = json.loads(response.content)
 
     assert response.status_code == 200
     assert data['response'] == "service alive"
 
 def test_hello():        
-    response = requests.get('https://{}/{}'.format(ip, new_name), verify=False)
+    response = requests.get('https://{}/{}'.format(domain, new_name))
     data = json.loads(response.content)
 
     assert response.status_code == 200
     assert data['response'] == "nice to meet you {}".format(new_name)
 
 def test_hello_2():        
-    response = requests.get('https://{}/{}'.format(ip, new_name), verify=False)
+    response = requests.get('https://{}/{}'.format(domain, new_name))
     data = json.loads(response.content)
 
     assert response.status_code == 200
